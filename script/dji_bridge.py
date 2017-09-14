9c3a0d9462e6c53d64e47df16d2e90c7dc65f1f8
Andy Smith <smithan7@oregonstate.edu>
2017-09-14 00:44:50 -0700

    params

--- script/dji_bridge.py
+++ script/dji_bridge.py
@@ -19,22 +19,38 @@ import sys
 import math
 
 if __name__ == "__main__":
-	# hardware test environment
-	#nw_corner = [44.539847, -123.251004]#rospy.get_param( '~nw_corner' )
-	#se_corner = [44.538552, -123.247446]#rospy.get_param( '~se_corner' )
+	drone = DJIDrone()
+	env_num = rospy.get_param('test_environment_number')
+
+	nw_corner = []
+	se_corner = []
 
 	# practice field
-	#nw_corner = [44.565683, -123.272974]
-	#se_corner = [44.564965, -123.270456]
+	if env_num == 0:
+		nw_corner = [44.565683, -123.272974]
+		se_corner = [44.564965, -123.270456]
+
+
+	# hardware test environment
+	if env_num == 3:	
+		nw_corner = [44.539847, -123.251004]#rospy.get_param( '~nw_corner' )
+		se_corner = [44.538552, -123.247446]#rospy.get_param( '~se_corner' )
+
 
 	# new hardware4
-	nw_corner = [44.5394569, -123.250866]
-	se_corner = [44.538162, -123.247776]
+	if env_num == 4:	
+		nw_corner = [44.5394569, -123.250866]
+		se_corner = [44.538162, -123.247776]
+
+	# new hardware4
+	if env_num == 5:	
+		nw_corner = [44.539580, -123.250876]
+		se_corner = [44.538725, -123.247601]
 
 	#rospy.init_node("DJI_Bridge")
 	#Q = quad( nw_corner, se_corner )	
 	print("DJI_Bridge::Initializing")
-	drone = DJIDrone()
+	
 	Q = quad( drone, nw_corner, se_corner )
 	
 	rospy.spin()
